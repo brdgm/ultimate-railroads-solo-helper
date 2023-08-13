a1713f8c53c93218aea7c871cb40becdcb3a78ee
Stefan Seifert <stefanseifert@users.noreply.github.com>
2023-08-13 08:41:34 +0200

    improve track label

--- src/components/round/SideBar.vue
+++ src/components/round/SideBar.vue
@@ -2,7 +2,7 @@
   <div class="sidebar">
     <p>{{t('sideBar.round')}} <strong>{{round}}</strong> / {{navigationState.numberOfRounds}}</p>
     <button class="btn btn-outline-secondary btn-sm" v-html="t('sideBar.rules.title')" data-bs-toggle="modal" data-bs-target="#rulesModal"></button>
-    <h5 class="mt-3" v-html="t('sideBar.tracks')"></h5>
+    <h6 class="mt-3" v-html="t('sideBar.tracks')"></h6>
     <div v-for="(track) of allTracks" :key="track + isAvailable(track)" class="icon-container">
       <AppIcon :type="isAvailable(track) ? 'track' : 'track-unavailable'" :name="track" @click="toggle(track)"
           class="icon" :class="{[track]:true,toggle:isAllowToggle(track)}" />
